7d8849fb1fcf5782e8ee4fcbf0e1f95cf3adbe81
ChiaraBozz <chiarabozz00@gmail.com>
2024-01-13 16:26:57 +0100

    c

--- lesson_5/01_PrefixScan/PrefixScan.cu
+++ lesson_5/01_PrefixScan/PrefixScan.cu
@@ -19,28 +19,8 @@ using namespace timer;
 
 //const int BLOCK_SIZE = 4;
 
-__global__ void PrefixScan123(int* VectorIN, int N) {
-	//int offset;
-	int i = blockDim.x * blockIdx.x + threadIdx.x;
-	if(i < N){
-		for(int level = 0; level < log2f(N); ++level){
-			//printf("%f \n", floor(log2f(N)));
-			int offset = pow(2, level); //2^level
-			
-			int index = i + offset;
-
-            if (index < N) {
-                int temp = VectorIN[index - offset];
-                __syncthreads();
-                VectorIN[index] += temp;
-            }
-
-            __syncthreads();
-        }
-	}
-}
-
-__global__ void PrefixScan(int* VectorIN, int N) {
+// It works properly only if BLOCK_SIZE < N
+__global__ void PrefixScan_1(int* VectorIN, int N) {
 	//int offset;
 	int i = blockDim.x * blockIdx.x + threadIdx.x;
 	if(i < N)
@@ -58,35 +38,46 @@ __global__ void PrefixScan(int* VectorIN, int N) {
                 VectorIN[i] = temp + temp1;
             }
             __syncthreads();
-
-			/*if(i >= offset)
-				VectorIN[i] = VectorIN[i - offset] + VectorIN[i];
-			__syncthreads();
-			
-			if(threadIdx.x == 0 && blockIdx.x == 0){
-				for (int i = 0; i < N; ++i){
-					printf("%i ", VectorIN[i]);
-				} printf("\n");
-			}*/
 		}
 }
 
-__global__ void PrefixScan1(int* VectorIN, int N) {
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
+__global__ void PrefixScan(int* VectorIN, int N, int* VectorAdd) {
+	//int offset;
+	__shared__ int shMem[1024];
+ 	int tid = blockIdx.x * blockDim.x + threadIdx.x;
 
-    if (i < N) {
-        for (int stride = 1; stride < N; stride *= 2) {
-            int index = i + stride;
+	int i = threadIdx.x;
 
-            if (index < N) {
-                int temp = VectorIN[index - stride];
-                __syncthreads();
-                VectorIN[index] += temp;
+	shMem[threadIdx.x] = VectorIN[tid];
+
+	if(tid < N){
+		for(int offset = 1; offset < blockDim.x; offset*=2){
+			if (i >= offset) {
+                int temp = shMem[i];
+				int temp1 = shMem[i-offset];
+                //__syncthreads(); // Synchronize before updating VectorIN[i]
+                shMem[i] = temp + temp1;
             }
-
             __syncthreads();
-        }
-    }
+		}
+		VectorIN[tid] = shMem[threadIdx.x];
+		if(i == blockDim.x -1)
+			VectorAdd[blockIdx.x] = shMem[i];
+	}
+}
+__global__ void EndPrefixScan(int* VectorIN, int N, int* VectorADD) {
+	int tid = blockDim.x * blockIdx.x + threadIdx.x;
+	//int i = (tid) * (blockIdx.x);
+	if(blockIdx.x > 0){
+		int toSum = VectorADD[blockIdx.x-1];
+	
+		if(tid<N)
+			//printf("%i : %i, ", tid, blockIdx.x);
+			if(blockIdx.x > 0){
+				//printf("\n%i : %i + %i\n", tid, VectorIN[tid], toSum);
+				VectorIN[tid]+= toSum;
+			}
+	}
 }
 
 
@@ -107,14 +98,13 @@ int main(int argc, char *argv[]) {
 
     const int N = std::atoi(argv[1]);
     const int BLOCK_SIZE = std::atoi(argv[2]);
-
 	const int blockDim = BLOCK_SIZE;
+	const int GridDim = ((N + blockDim - 1) / (blockDim));
 	//const int N = BLOCK_SIZE * 131072;
 	// ------------------- INIT ------------------------------------------------
 
     // Random Engine Initialization
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-	seed = 0;
     std::default_random_engine generator (seed);
     //std::uniform_int_distribution<int> distribution(1, 100);
 	std::uniform_int_distribution<int> distribution(1, 10);
@@ -124,25 +114,55 @@ int main(int argc, char *argv[]) {
 	Timer<HOST> dev_TM;
 
 	// ------------------ HOST INIT --------------------------------------------
-
+	
 	int* VectorIN = new int[N];
 	for (int i = 0; i < N; ++i)
 		VectorIN[i] = distribution(generator);
 
 	// ------------------- CUDA INIT -------------------------------------------
-
+	int* VectorADD = new int[GridDim];
+	int* VectorADD1 = new int[GridDim];
 	int* devVectorIN;
+	int* devVectorADD;
 	SAFE_CALL( cudaMalloc(&devVectorIN, N * sizeof(int)) );
+    SAFE_CALL( cudaMalloc(&devVectorADD, GridDim * sizeof(int)) );
     SAFE_CALL( cudaMemcpy(devVectorIN, VectorIN, N * sizeof(int), cudaMemcpyHostToDevice) );
 
 	int* prefixScan = new int[N];
 	float dev_time;
 
 	// ------------------- CUDA COMPUTATION 1 ----------------------------------
-	int GridDim = ((N + blockDim - 1) / (blockDim));
+	
 
 	dev_TM.start();
-	PrefixScan<<< GridDim, blockDim>>>(devVectorIN, N);
+	PrefixScan<<< GridDim, blockDim>>>(devVectorIN, N, devVectorADD);
+	
+	cudaDeviceSynchronize();
+	//SAFE_CALL(cudaMemcpy(prefixScan, devVectorIN, N * sizeof(int),
+    //                       cudaMemcpyDeviceToHost) );
+	//printArray(prefixScan, N, "2GP");
+
+	printf("GridDim: %i\n", GridDim);
+	if(GridDim > 1){
+		SAFE_CALL(cudaMemcpy(VectorADD, devVectorADD, GridDim * sizeof(int),cudaMemcpyDeviceToHost) );
+		
+		//printArray(VectorADD, GridDim, "VAD");
+		std::partial_sum(VectorADD, VectorADD + GridDim, VectorADD1);
+		
+		//printArray(VectorADD1, GridDim, "VA1");
+		SAFE_CALL( cudaMemcpy(devVectorADD, VectorADD1, GridDim * sizeof(int), cudaMemcpyHostToDevice) );
+		EndPrefixScan<<< GridDim, blockDim>>>(devVectorIN, N, devVectorADD);
+	}
+	/*
+	if(GridDim > 1){
+		SAFE_CALL(cudaMemcpy(VectorADD, devVectorADD, NUM_BLOCKS * sizeof(int),cudaMemcpyDeviceToHost) );
+		std::partial_sum(VectorADD, VectorADD + NUM_BLOCKS, VectorADD1);
+		//int NewGridDim = ((NUM_BLOCKS + blockDim - 1) / (blockDim));
+		//PrefixScan<<< NewGridDim, blockDim>>>(devVectorADD, NUM_BLOCKS, devVectorADD1); // -> an illegal memory access was encountered(700)
+		SAFE_CALL( cudaMemcpy(devVectorADD1, VectorADD1, NUM_BLOCKS * sizeof(int), cudaMemcpyHostToDevice) );
+		EndPrefixScan<<<GridDim, blockDim>>>(devVectorIN, N, devVectorADD1);
+	}*/
+
 	dev_TM.stop();
 	dev_time = dev_TM.duration();
 
@@ -164,10 +184,10 @@ int main(int argc, char *argv[]) {
     host_TM.stop();
 
 	
-	/*printArray(VectorIN, N, "Initial");
-	printArray(host_result, N, "CPU");
-	printArray(prefixScan, N, "GPU");
-	*/
+	//printArray(VectorIN, N, "INI");
+	//printArray(host_result, N, "CPU");
+	//printArray(prefixScan, N, "GPU");
+	
 	int flag = 0;
 	for (int i = 0; i < N; ++i)
 		if(host_result[i] != prefixScan[i]){
@@ -181,13 +201,7 @@ int main(int argc, char *argv[]) {
 		std::cerr << " Error! :  prefixScan" << std::endl << std::endl;
 		cudaDeviceReset();
 		std::exit(EXIT_FAILURE);
-	}
-	/*if (!std::equal(host_result, host_result + blockDim - 1, prefixScan + 1)) {
-		std::cerr << " Error! :  prefixScan" << std::endl << std::endl;
-		cudaDeviceReset();
-		std::exit(EXIT_FAILURE);
-	}*/
-	
+	}	
     // ----------------------- SPEEDUP -----------------------------------------
 
     float speedup1 = host_TM.duration() / dev_time;
--- lesson_5/01_PrefixScan/compile.sh
+++ lesson_5/01_PrefixScan/compile.sh
@@ -3,5 +3,5 @@ DIR=`dirname $0`
 
 nvcc -w -std=c++11 -arch=sm_61 "$DIR"/PrefixScan.cu -I"$DIR"/include -o prefixscan
 nvcc -w -std=c++11 -arch=sm_61 "$DIR"/PrefixScanv2.cu -I"$DIR"/include -o workprefixscan
-nvcc -w -std=c++11 -arch=sm_61 "$DIR"/PrefixScanv2.cu -I"$DIR"/include -o workprefixscan
+nvcc -w -std=c++11 -arch=sm_61 "$DIR"/PrefixScanv2shmem.cu -I"$DIR"/include -o shmworkprefixscan
 nvcc -w -std=c++11 -arch=sm_61 "$DIR"/PrefixScanv3.cu -I"$DIR"/include -o taskparprefixscan
